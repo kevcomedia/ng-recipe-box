eec3b956d657046bd6387efc47d04739aad8b334
Kev Comedia <kevcomedia@gmail.com>
2017-07-14 21:59:29 +0800

    Create recipe list on local storage if none is found
    This is mostly just a test of setting a value in local storage when the
    app loads. Of course we could keep the initialization function in the
    future.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeService } from './recipe.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/recipe.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe';
+
+@Injectable()
+export class RecipeService {
+  private localStorageKey = '_gckev_ng-recipes';
+
+  /**
+   * Creates a stringified array of recipes on local storage if none is found.
+   */
+  initializeRecipeList(): void {
+    if (!localStorage.getItem(this.localStorageKey)) {
+      const initialRecipes: Recipe[] = [
+        {
+          name: 'Fluffy Rice',
+          ingredients: ['rice', 'water']
+        },
+        {
+          name: 'Pork Adobo',
+          ingredients: ['pork', 'vinegar', 'soy sauce', 'onions', 'oil']
+        }
+      ];
+
+      localStorage
+        .setItem(this.localStorageKey, JSON.stringify(initialRecipes));
+    }
+  }
+}
